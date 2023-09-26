34b04c591194e833c09cc439bba19cc3a9da7591
Pelmen <Pelmen@DESKTOP-FCPDJ33>
2023-09-26 16:38:24 +0300

    Добавлена страница регистрации

---
+++ ReaderDiary/pages/Registration.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReaderDiary.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void ToRegistration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
