4a4f7d72f7903ae0e917bc300cb563b5d94eb8be
Pelmen <Pelmen@DESKTOP-FCPDJ33>
2023-10-03 16:47:19 +0300

    не работает

---
+++ ReaderDiary/pages/AdminMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReaderDiary.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminMenu.xaml
+    /// </summary>
+    public partial class AdminMenu : Page
+    {
+        public AdminMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ToShowUsers_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("pages/AllUsers.xaml", UriKind.Relative));
+        }
+    }
+}
--- ReaderDiary/pages/Login.xaml.cs
+++ ReaderDiary/pages/Login.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ReaderDiary.classes;
+using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
@@ -17,7 +19,22 @@ namespace ReaderDiary.pages
 
         private void Enter_Click(object sender, RoutedEventArgs e)
         {
-
+            Account user = Base.RDBase.Account.FirstOrDefault(x => x.login.ToString() == login.Text && x.password.ToString() == password.Text);
+            if (user != null)
+            {
+                switch (user.id_role)
+                {
+                    case 1:
+                        NavigationService.Navigate(new Uri("pages/AdminMenu.xaml", UriKind.Relative));
+                        break;
+                        case 2:
+                        MessageBox.Show("Юзер");
+                        break;
+                    default:
+                        MessageBox.Show("Не найден");
+                        break;
+                }
+            }
         }
 
         private void GoToRegistration_Click(object sender, RoutedEventArgs e)
