8643d44124450d15b96c6e45f4bfb1808bc07de7
Pelmen <Pelmen@DESKTOP-FCPDJ33>
2023-10-07 14:48:52 +0300

    Как надо, но уродливо

--- ReaderDiary/pages/AllUsers.xaml.cs
+++ ReaderDiary/pages/AllUsers.xaml.cs
@@ -25,27 +25,7 @@ namespace ReaderDiary.pages
         {
             InitializeComponent();
 
-            List<Account> accounts = Base.RDBase.Account.ToList();
-            List<Gender> genders = Base.RDBase.Gender.ToList();
-            List<UserData> userDatas = Base.RDBase.UserData.ToList();
-            List<UserRole> userRoles = Base.RDBase.UserRole.ToList();
-
-            var users = from gender in genders
-                        join account in accounts on gender.id_gender equals account.id_gender
-                        join datas in userDatas on account.id_user equals datas.id_user
-                        join roles in userRoles on account.id_role equals roles.id_role
-                        select new
-                        {
-                            login = account.login,
-                            name = datas.name,
-                            surname = datas.surname,
-                            patronymic = datas.patronymic,
-                            birthday = account.birthday,
-                            gender = gender.title,
-                            roles = roles.title,
-                        };
-
-            ListUsers.ItemsSource = users;
+            Users.ItemsSource = Base.RDBase.UserData.ToList();
         }
     }
 }
