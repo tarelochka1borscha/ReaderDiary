4d6ba806ffd25fe5796a0d8c62cca83b94ecf7f1
Pelmen <Pelmen@DESKTOP-FCPDJ33>
2024-03-04 13:24:14 +0300

    пофикшена сортировка

--- ReaderDiary/pages/AllUsers.xaml.cs
+++ ReaderDiary/pages/AllUsers.xaml.cs
@@ -21,48 +21,88 @@ namespace ReaderDiary.pages
     /// </summary>
     public partial class AllUsers : Page
     {
+        List<UserData> data = Base.RDBase.UserData.ToList();
+        bool up = false;
+        bool down = false;
+        bool man = false;
+        bool woman = false;
         public AllUsers()
         {
             InitializeComponent();
 
-            Users.ItemsSource = Base.RDBase.UserData.ToList();
+            Users.ItemsSource = data;
         }
 
         private void Search_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (Search.Text != string.Empty)
             {
-                Users.ItemsSource = Base.RDBase.UserData.Where(x=> x.surname.ToLower().Contains(Search.Text.ToLower()) || x.Account.login.ToLower().Contains(Search.Text.ToLower())).ToList();
+                data = data.Where(x=> x.surname.ToLower().Contains(Search.Text.ToLower()) || x.Account.login.ToLower().Contains(Search.Text.ToLower())).ToList();
             }
             else
             {
-                Users.ItemsSource = Base.RDBase.UserData.ToList();
+                if (up)
+                {
+                    data = Base.RDBase.UserData.OrderBy(x => x.surname).ToList();
+                }
+                if (down)
+                {
+                    data = Base.RDBase.UserData.OrderByDescending(x => x.surname).ToList();
+                }
+                if (man)
+                {
+                    data = Base.RDBase.UserData.Where(x => x.Account.id_gender == 1).ToList();
+                }
+                if (woman)
+                {
+                    data = Base.RDBase.UserData.Where(x => x.Account.id_gender == 2).ToList();
+                }
             }
+
+            Users.ItemsSource = data;
         }
 
         private void Upp_Click(object sender, RoutedEventArgs e)
         {
-            Users.ItemsSource = Base.RDBase.UserData.OrderBy(x=> x.surname).ToList();
+            data = data.OrderBy(x => x.surname).ToList();
+            Users.ItemsSource = data;
+            up = true;
+            down = false;
         }
 
         private void Low_Click(object sender, RoutedEventArgs e)
         {
-            Users.ItemsSource = Base.RDBase.UserData.OrderByDescending(x => x.surname).ToList();
+            data = data.OrderByDescending(x => x.surname).ToList();
+            Users.ItemsSource = data;
+            up = false;
+            down = true;
         }
 
         private void Man_Click(object sender, RoutedEventArgs e)
         {
-            Users.ItemsSource = Base.RDBase.UserData.Where(x => x.Account.id_gender==1).ToList();
+            data = data.Where(x => x.Account.id_gender == 1).ToList();
+            Users.ItemsSource = data;
+            man = true;
+            woman = false;
         }
 
         private void Woman_Click(object sender, RoutedEventArgs e)
         {
-            Users.ItemsSource = Base.RDBase.UserData.Where(x => x.Account.id_gender == 2).ToList();
+            data = data.Where(x => x.Account.id_gender == 2).ToList();
+            Users.ItemsSource = data;
+            man = false;
+            woman = true;
         }
 
         private void Reset_Click(object sender, RoutedEventArgs e)
         {
-            Users.ItemsSource = Base.RDBase.UserData.ToList();
+            Search.Clear();
+            data = Base.RDBase.UserData.ToList();
+            bool up = false;
+            bool down = false;
+            bool man = false;
+            bool woman = false;
+            Users.ItemsSource = data;
         }
     }
 }
